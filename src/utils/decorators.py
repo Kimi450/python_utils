2fa8b1d09460ef01c4bf2692e08ae364fad89a53
Karan Samani <karan.samani@ericsson.com>
2022-06-30 14:47:41 +0100

    Updates to exit on none decorator

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -30,10 +30,21 @@ def log(description=None, trim=True, level=logging.INFO, _func=None, *, my_logge
     '''
     Decorator to log function calls and their parameters
     https://ankitbko.github.io/blog/2021/04/logging-in-python/
+
+    @log(...)
+    def method()..
+    
+    @log
+    def method()..
+    
+    @log([...]) #not recommended, keep this closest to method
+    @another
+    def method()..
     '''
     def decorator_log(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            # Get default logger or something else
             if my_logger is None:
                 logger = get_default_logger()
             else:
@@ -42,6 +53,7 @@ def log(description=None, trim=True, level=logging.INFO, _func=None, *, my_logge
                 else:
                     logger = my_logger
 
+            # Get the method signature ready
             args_dict = dict(zip(getargspec(func).args, [re.sub(r'[\']', '', repr(a)) for a in args]))
             
             if trim:
@@ -54,7 +66,7 @@ def log(description=None, trim=True, level=logging.INFO, _func=None, *, my_logge
             
             args_repr = []
             for key, value in args_dict.items():
-                value = re.sub("\s\s+", " ", value) if trim else value
+                value = _trim_whitespace_if_required(trim, value)
                 data = f"{key}={value}"
                 args_repr.append(data)
             
@@ -62,6 +74,7 @@ def log(description=None, trim=True, level=logging.INFO, _func=None, *, my_logge
 
             signature = ", ".join(args_repr + kwargs_repr)
 
+            # Add extra logs specific in the function, if applicable
             extra_logs = ""
             try:
                 extra_logs = ""
@@ -71,15 +84,18 @@ def log(description=None, trim=True, level=logging.INFO, _func=None, *, my_logge
                     extra_logs = args_dict["extra_logs"] + " - "
             except:
                 pass
+
+            # get the logging method and call it
             callback = _get_logging_level(logger, level)
             callback(f"{extra_logs}{description}: {func.__name__}({signature})")
+
             try:
                 result = func(*args, **kwargs)
                 callback(f"{extra_logs}{description}: {func.__name__}({signature}) completed successfully!")
-                return result
             except Exception as e:
                 logger.exception(f"Exception raised in '{func.__name__}': {str(e)}")
                 raise e
+            return result
         return wrapper
 
     if _func is None:
@@ -87,15 +103,48 @@ def log(description=None, trim=True, level=logging.INFO, _func=None, *, my_logge
     else:
         return decorator_log(_func)
 
-def exit_on_none(function):
-    '''Wrapper to exit script with err code 1 if function returns None'''
-    def wrapper(*args, **kwargs):
-        data = function(*args, **kwargs)
-        if data == None:
-            exit(1)
-        return data
-    return wrapper
+def exit_on_none(description="EXITING", level=logging.ERROR, _func=None, *, my_logger: Union[CustomLogger, logging.Logger] = None):
+    '''
+    Wrapper to exit script with exit code 450 if function returns None
+    
+    Calling way:
+    
+    @exit_on_none(...)
+    def method()..
+    
+    @exit_on_none
+    def method()..
+    
+    @exit_on_none([...])
+    @another
+    def method()..
+    '''
+    def just_a_function_to_use_functools_for_the_nested_function(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Get default logger or something else
+            if my_logger is None:
+                logger = get_default_logger()
+            else:
+                if isinstance(my_logger, CustomLogger):
+                    logger = my_logger.get_logger(func.__name__)
+                else:
+                    logger = my_logger
 
+            data = func(*args, **kwargs)
+
+            if data == None:
+                # get the logging method and call it
+                callback = _get_logging_level(logger, level)
+                callback(f"{description}: '{func.__name__}(...)' exitted with a None. Exiting with error code 450.")
+                exit(450)
+            return data
+        return wrapper
+
+    if _func is None:
+        return just_a_function_to_use_functools_for_the_nested_function
+    else:
+        return just_a_function_to_use_functools_for_the_nested_function(_func)
 
 if __name__ == "__main__":
     @log(description="POSTing to data catalog")
